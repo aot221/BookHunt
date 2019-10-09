07b3ff9784029940e93f51ce54da9648295b4d54
a <aot221@nyu.edu>
2019-10-09 12:59:04 -0400

    fix port

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 Consumes google books api to deliver a seemsless book searching experience.
+
+Directions to install and run:
+
+1. Unzip contents into empty directory.
+2. "npm i" to install dependencies.
+3. "npm start" to run application on localhost port 3000.
--- public/js/googleBooks.js
+++ public/js/googleBooks.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
 	var tableData = document.getElementById("google-book-table"); 
     console.log( "ready!" );
+
+    //Event for when search button is clicked
 	$("#book-search-submit").click(function(){		
-		//
 		var searchTerms = $("#book-search").val();
 		console.log(searchTerms);
 		searchUrl = 'https://www.googleapis.com/books/v1/volumes?q=' + searchTerms;
@@ -13,7 +14,6 @@ $(document).ready(function() {
 			url:searchUrl,
 			success: function(json){
 				console.log(json);
-				//var htmlcontent = "";
 				//loop through content of json response from Google Books api and display as list.
 				var table = `
 					<table class="table table-striped" id = "myTable">
@@ -43,19 +43,7 @@ $(document).ready(function() {
 					  </table>`
 					  ;
 
-					tableData.innerHTML = table;
-
-				// for (i = 0; i < json.items.length; i++) 		
-		  // 		{
-		  // 			tr = $('<tr/>');
-		  //           tr.append("<td>" + json.items[i].volumeInfo.title + "</td>");
-		  //           tr.append("<td>" + json.items[i].volumeInfo.authors[0] + "</td>");
-		  //           tr.append("<td>" + json.items[i].volumeInfo.publisher + "</td>");
-		  //           tr.append("<td>" + "<img src=" + json.items[i].volumeInfo.imageLinks.thumbnail + ">" + json.items[i].volumeInfo.publisher + "</td>");
-		      //       $('table').append(tr);
-		   			// htmlcontent += "<li>Title: " + json.items[i].volumeInfo.title + "&nbsp Author: " + json.items[i].volumeInfo.authors[0] + "&nbsp Publisher: " + json.items[i].volumeInfo.publisher + "<img src=" + json.items[i].volumeInfo.imageLinks.thumbnail +  ">" + "<br>";
-		  		//}
-				//document.getElementById("books").innerHTML = "<ul>" + htmlcontent + "</ul><br>";				   
+					tableData.innerHTML = table;			   
 			}
 		});
 	});    
