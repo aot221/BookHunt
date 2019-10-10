5938e1534a0edb513916c771182562c0a131efaf
a <aot221@nyu.edu>
2019-10-10 17:24:50 -0400

    removed unused code

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ app.use(express.static('public'));
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist/'));
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css/'));
 
-//Run web server for application on a designated port. Currently set up to work on localhost.
+//Run web server for application on a designated port.
 app.listen(port, () => console.log(`Google Books Search App listening on port ${port}!`));
